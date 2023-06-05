96d5fcb4bde9cc653fa2ae1377b2868f3f3dc19f
Jim Dines <jim.dines@boomi.com>
2023-06-05 20:12:29 +0000

    Updated deleteDeployedPackage.sh

--- cli/scripts/bin/deleteDeployedPackage.sh
+++ cli/scripts/bin/deleteDeployedPackage.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
 
 source bin/common.sh 
-# Query processattachment id before creating it
-echov "$@"
-
 
 # mandatory arguments
 ARGUMENTS=(deploymentId)
-JSON_FILE=json/deleteDeployedPackage.json
-URL=$baseURL/DeployedPackage/${deploymentId}
-id=deploymentId
 inputs "$@"
 if [ "$?" -gt "0" ]
 then
         return 255;
 fi
- 
-deleteAPI
+echov "$@"
 
+URL=$baseURL/DeployedPackage/${deploymentId} 
+deleteAPI
 clean
 if [ "$ERROR" -gt "0" ]
 then
